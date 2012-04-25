0e3ef340f80f49c68fcc15792e511cf6d0a4c04b
Kyle Burton <kyle.burton@gmail.com>
2012-04-24 22:07:00 -0400

    bump version for dev, specify release config

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject lein-release/lein-release "1.0.0"
+(defproject lein-release/lein-release "1.0.1-SNAPSHOT"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in :leiningen
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.3.0"]])
