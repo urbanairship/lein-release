0d0ce99a030fd2852b10d0641e29043d10cae0ec
Kyle Burton <kyle.burton@gmail.com>
2013-01-31 15:09:15 -0500

    lein-release plugin: preparing 1.0.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.2-SNAPSHOT"
+(defproject lein-release/lein-release "1.0.2"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in :leiningen
