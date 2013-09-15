5a123018e0124eb8c45bf9cab2b96e7152827314
Kyle Burton <kyle.burton@gmail.com>
2013-09-15 12:04:19 -0400

    lein-release plugin: bumped version from 1.0.5 to 1.0.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.5"
+(defproject lein-release/lein-release "1.0.6-SNAPSHOT"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in :leiningen
