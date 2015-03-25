b3561f7f7cf5481f7f36b4621787b5e4ee3bbd91
Ross Donaldson <gastove@gmail.com>
2015-03-25 15:04:50 -0700

    Absolutely no reason I can think of to depend on swank

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-release/lein-release "1.0.6-SNAPSHOT"
   :description "Leiningen Release Plugin"
-  :dev-dependencies [[swank-clojure "1.4.2"]]
+;  :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in :leiningen
   :lein-release {:deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
